d31eca53be9fce9c6ee856a051e39b78b9d14fa8
KangWooJu <space1102@naver.com>
2025-01-12 05:29:20 +0900

    fix : SecurityConfig 수정

--- src/main/java/org/bbiak/skeleton_user/Global/Security/Config/SecurityConfig.java
+++ src/main/java/org/bbiak/skeleton_user/Global/Security/Config/SecurityConfig.java
@@ -56,15 +56,15 @@ public class SecurityConfig {
                 .cors((corsCustomizer)-> corsCustomizer.configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                       CorsConfiguration configuration = new CorsConfiguration();
+                        CorsConfiguration configuration = new CorsConfiguration();
 
-                       configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                       configuration.setAllowedMethods(Collections.singletonList("*"));
-                       configuration.setAllowCredentials(true);
-                       configuration.setAllowedHeaders(Collections.singletonList("*"));
-                       configuration.setMaxAge(3600L);
-                       configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-                       return configuration;
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+                        return configuration;
                     }
                 }));
 
